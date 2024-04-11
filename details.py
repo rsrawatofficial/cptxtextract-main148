5531c8f3d82ea59b569665d9ef1411c01dd10874
hemendrasahu67 <57944144+hemendrasahu67@users.noreply.github.com>
2024-04-11 09:00:23 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+import os
+
+api_id = 27006142
+api_hash = "8d5171c23fd1836db908b8739d288336"
+bot_token = "7071754788:AAEU_gL99tEWgs3WM2qnRALvhmGV3Q8HtBA"
+auth_users = [1798290309]
+sudo_user = 1798290309
+log_channel = -100212046048
+txt_channel = -100212046048
