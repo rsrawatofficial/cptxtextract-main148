4c4255bdf31d79f71f03bce96122d6a6dfd2cbf5
Rohanthakur360 <70991755+Rohanthakur360@users.noreply.github.com>
2024-04-14 21:48:04 +0530

    Update details.py

--- details.py
+++ details.py
@@ -3,7 +3,7 @@ import os
 api_id = 27006142
 api_hash = "8d5171c23fd1836db908b8739d288336"
 bot_token = "7071754788:AAEU_gL99tEWgs3WM2qnRALvhmGV3Q8HtBA"
-auth_users = [1798290309]
-sudo_user = 1798290309
-log_channel = -100212046048
-txt_channel = -100212046048
+auth_users = [1871785273]
+sudo_user = 1871785273
+log_channel = -1002132785565
+txt_channel = -1002132785565
