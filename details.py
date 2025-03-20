92170a19e676a0c2a48bcc03b43c5b948e785d43
BharatChhimpa <bhartlal2013@gmail.com>
2025-03-20 20:26:10 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 27006142
-api_hash = "8d5171c23fd1836db908b8739d288336"
-bot_token = "7071754788:AAEU_gL99tEWgs3WM2qnRALvhmGV3Q8HtBA"
-auth_users = [1871785273]
-sudo_user = 1871785273
-log_channel = -1002132785565
-txt_channel = -1002132785565
+api_id = 29683927
+api_hash = "b2eb32ac1030edd8bc36c7b554ef6fc3"
+bot_token = 7903348261:AAFgADxgmNQUiW9UhF_0srnTFCKWT6RLW90"
+auth_users = [5583858510]
+sudo_user = 5583858510
+log_channel = -2311732949
+txt_channel = -2311732949
