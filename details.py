c8b5f2bc2c4bd7e60444aa785edb2e4b5eb17896
BharatChhimpa <bhartlal2013@gmail.com>
2025-03-20 20:26:44 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 29683927
 api_hash = "b2eb32ac1030edd8bc36c7b554ef6fc3"
-bot_token = 7903348261:AAFgADxgmNQUiW9UhF_0srnTFCKWT6RLW90"
+bot_token = "7903348261:AAFgADxgmNQUiW9UhF_0srnTFCKWT6RLW90"
 auth_users = [5583858510]
 sudo_user = 5583858510
 log_channel = -2311732949
