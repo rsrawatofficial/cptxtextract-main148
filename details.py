ff1d8a89dbc3536e8917f3fc573c61864b845d0e
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-11 15:13:27 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 29683927
-api_hash = "b2eb32ac1030edd8bc36c7b554ef6fc3"
-bot_token = "7903348261:AAFgADxgmNQUiW9UhF_0srnTFCKWT6RLW90"
-auth_users = [5583858510]
-sudo_user = 5583858510
-log_channel = -2311732949
-txt_channel = -2311732949
+api_id = 27900743
+api_hash = "ebb06ea8d41420e60b29140dcee902fc""
+bot_token = "7609528498:AAGqcl_h-Cygc4v_oiXqD4cbp-yGypVcakY"
+auth_users = [7804396225]
+sudo_user = 7804396225
+log_channel = -1002586276615
+txt_channel = -1002586276615
